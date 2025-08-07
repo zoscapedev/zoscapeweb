5e2f37310411b14bd3c780bbfa15d43b9ac347e6
omchillure <omchillure4@gmail.com>
2025-08-07 16:36:52 +0530

    stripe fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: "2024-09-30.acacia",
+  apiVersion: "2025-02-24.acacia",
   typescript: true,
 })
